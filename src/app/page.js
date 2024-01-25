d5643186a5224d452487486cc719790c9ac34363
mindset001 <58329098+mindset001@users.noreply.github.com>
2024-01-25 23:50:15 +0100

    newk

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Logo from "../../public/images/logo.png"
 import Left from "../../public/images/left.png"
 import Right from "../../public/images/right.png"
+import Left2 from "../../public/images/left2.png"
+import Right2 from "../../public/images/right2.png"
 import Ph from "../../public/images/ph.png"
 export default function Home() {
   return (
@@ -12,16 +14,20 @@ export default function Home() {
       </div>
       <div className="w-full flex justify-center items-center h-[80vh] md:h-[80vh] text-center">
         <div>
-        <h1 className="w-[361px]md:w-[940px] text-[28px] md:text-[56px] font-[700] text-center text-[#003333]">We’re cooking our Website for you!</h1>
-       <div className="flex justify-center">
+        <h1 className="w-[361px] md:w-[940px] text-[28px] lg:text-[56px] font-[700] text-center text-[#003333]">We’re cooking our Website for you!</h1>
+       <div className="flex justify-center w-full">
        <p className=" w-[300px] text-[12px] md:text-[20px] font-[400] md:w-[900px]">Errandboi provides a convenient solution for busy individuals by allowing them to order fresh and raw food items through a user-friendly web application.</p>
        </div>
         </div>
       </div>
       <div>
-        <div className="flex justify-between px-8">
-          <Image src={Left}/>
+        <div className="hidden md:flex justify-between px-8">
+          <Image src={Left} className=""/>
           <Image src={Right}/>
+        </div>
+        <div className="flex justify-between px-8 md:hidden">
+          <Image src={Left2}/>
+          <Image src={Right2}/>
         </div>
         <div className="flex justify-center w-full">
           <div>
